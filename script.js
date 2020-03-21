e7774ae16cd032c0a1af8dea761c1cb2e31630ba
Meghana <53141586+lathamithu@users.noreply.github.com>
2020-03-21 17:09:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,22 +9,22 @@ $(document).ready(function(){
     function get(){
         $('#Body').html('');
         $.ajax({
-            url: 'http://localhost:3000/api/tutorials',
+            url: 'http://fakerestapi.azurewebsites.net',
             method: 'get',
             dataType: 'json',
             data: {
                 test: 'test data'
             },
             success: function(data) {
-                $(data).each(function(i, e){
+                $(data).each(function(i, tutorial){
                     $('#Body').append($("<tr>")
-                                            .append($("<td>").append(e.todoNumber))
-                                            .append($("<td>").append(e.task))
-                                            .append($("<td>").append(e._id))
+                                            .append($("<td>").append(tutorial.Num))
+                                            .append($("<td>").append(tutorial.task))
+                                            .append($("<td>").append(tutorial._id))
                                             .append($("<td>").append(`
-                                                <i class="far fa-edit editTut" data-tutid="`+e._id+`"></i> 
-                                                <i class="fas fa-trash deleteTut" data-tutid="`+e._id+`"></i>
-                                            `)));
+                                            <i class="far fa-edit editTut" data-tutid="`+tutorial._id+`"></i> 
+                                            <i class="fas fa-trash deleteTut" data-tutid="`+tutorial._id+`"></i>
+                                        `)));
                     });
                 loadButtons();
                 }
@@ -33,13 +33,13 @@ $(document).ready(function(){
     
     function getOne(id){
         $.ajax({
-            url: 'http://localhost:3000/api/tutorials/' + id,
+            url: 'http://fakerestapi.azurewebsites.net' + '/getOne',
             method: 'get',
             dataType: 'json',
             success: function(data) {
                 $($("#updateForm")[0].Id).val(data._id);
-                $($("#updateForm")[0].updateNum).val(data.todoNumber);
-                $($("#updateForm")[0].updateType).val(data.type);
+                $($("#updateForm")[0].updateNum).val(data.Num);
+                $($("#updateForm")[0].updateTask).val(data.task);
                 $("#updateForm").show();
             }
         });
@@ -47,8 +47,8 @@ $(document).ready(function(){
     
     $("#submit").on("click", function(e) {
        let data = {
-           todoNumber: $($("#newForm")[0].Num).val(),
-           title: $($("#newForm")[0].task).val()
+           Num: $($("#newForm")[0].Num).val(),
+           task: $($("#newForm")[0].task).val()           
        } 
        
         post(data);
@@ -61,7 +61,7 @@ $(document).ready(function(){
     
     function post(data) {
         $.ajax({
-            url: 'http://localhost:3000/api/tutorials',
+            url: + 'http://fakerestapi.azurewebsites.net',
             method: 'POST',
             dataType: 'json',
             data: data,
@@ -73,20 +73,20 @@ $(document).ready(function(){
     }
     
     function loadButtons() {
-        $(".edit").click(function(e){
-            getOne($($(this)[0]).data("id"));
+        $(".editTut").click(function(e){
+            getOne($($(this)[0]).data("Id"));
             e.preventDefault();
         });
         
-        $(".delete").click(function(e){
-            Delete($($(this)[0]).data("id"));
+        $(".deleteTut").click(function(e){
+            Delete($($(this)[0]).data("Id"));
             e.preventDefault();
         })
     }
     
     function put(id, data){
         $.ajax({
-            url: 'http://localhost:3000/api/tutorials/' + id,
+            url: 'http://fakerestapi.azurewebsites.net' + '/put',
             method: 'PUT',
             dataType: 'json',
             data: data,
@@ -99,8 +99,8 @@ $(document).ready(function(){
     
     $("#update").on("click", function(e) {
        let data = {
-           todoNumber: $($("#updateForm")[0].updateNum).val(),
-           task: $($("#updateForm")[0].updateTask).val()
+           Num: $($("#updateForm")[0].updateNum).val(),
+           task: $($("#updateForm")[0].updateTask).val()           
        } 
        
         put($($("#updateForm")[0].Id).val(), data);
@@ -114,7 +114,7 @@ $(document).ready(function(){
     
     function Delete(id){
         $.ajax({
-            url: 'http://localhost:3000/api/tutorials/' + id,
+            url: 'http://fakerestapi.azurewebsites.net' + '/delete',
             method: 'DELETE',
             dataType: 'json',
             success: function(data) {
