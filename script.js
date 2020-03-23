a1d98e9b94198968043a861a2babe3ad86646c60
Meghana <53141586+lathamithu@users.noreply.github.com>
2020-03-23 15:11:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,9 @@
                     success: function( ){
                         
                         var txt2 = $("<ul></ul>").text( $('#list').val());
-                        $("body").append(txt2);
                         $("#list").val("");
+                        $("body").append((txt2).append('<button id="deletelist">DELETE</button>'));
+                                           
                         
                     },
                     error: function(errorThrown ){
@@ -29,8 +30,58 @@
                     method: 'DELETE',
                     dataType: 'json',
                     success: function(txt) {
-                        console.log(txt);
-                        getList(txt);
+                        
+                        $("#list").val("");
+                    }
+                });
+            });
+            function getList(data){
+                $.ajax({
+                    url: 'https://api.backendless.com/36DC4E7A-2B33-40D0-A382-FD197E23A89B/BC24AD0C-019C-42A0-BAAD-229B8D8F7222/data/CRUD',
+                    method: 'get',
+                    dataType: 'json',
+                    data : data,
+                    
+                });
+           
+            }
+            
+          });
+
+
+        $(document).ready(function(){
+            $("#addlist").click(function(){
+
+                var txt = JSON.stringify( { "List": ($('#list').val()) } ); 
+                $.ajax({ 
+                    url: 'https://api.backendless.com/36DC4E7A-2B33-40D0-A382-FD197E23A89B/BC24AD0C-019C-42A0-BAAD-229B8D8F7222/data/CRUD',
+                    type: 'post',
+                    contentType: 'application/json',
+                    data: txt,
+                    success: function( ){
+                        
+                        var txt2 = $("<ul></ul>").text( $('#list').val());
+                        $("#list").val("");
+                        $("body").append((txt2).append('<button id="deletelist">DELETE</button>'));
+                                           
+                        
+                    },
+                    error: function(errorThrown ){
+                        console.log( errorThrown );
+                    }
+                });
+
+            });
+                $("#deletelist").click(function(){
+                
+                var txt = JSON.stringify( { "List": ($('#list').val()) } ); 
+                $.ajax({
+                    url: 'https://api.backendless.com/36DC4E7A-2B33-40D0-A382-FD197E23A89B/BC24AD0C-019C-42A0-BAAD-229B8D8F7222/data/CRUD',
+                    method: 'DELETE',
+                    dataType: 'json',
+                    success: function(txt) {
+                        
+                        $("#list").val("");
                     }
                 });
             });
