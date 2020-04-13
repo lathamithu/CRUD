de32514b45321b5d0393153427d55489b62d09a4
Meghana <53141586+lathamithu@users.noreply.github.com>
2020-04-13 21:48:31 +0530

    Update main.js

--- Movies - Crud/main.js
+++ Movies - Crud/main.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
 
                 getList();
                 reset();
-                },           
+                },              
              
             error: function(error){
                 alert( error );
@@ -29,31 +29,14 @@ $(document).ready(function(){
 });
 
     
-function Delete()
-{   
-    var a='https://api.backendless.com/36DC4E7A-2B33-40D0-A382-FD197E23A89B/BC24AD0C-019C-42A0-BAAD-229B8D8F7222/data/Movies/3DFDFDD5-CEE9-DA09-FF86-87AA67C38900';
-    $.ajax({
-        url:a,
-        type: 'DELETE',
-        success:function(resp, status, xhr){
-            alert("Deleted");
-            console.log(resp, status, xhr);
-            getList();
-        },
-        error:function(xhr){
-            alert("error");
-            console.log(JSON.parse(xhr.responseText), xhr);
-            getList();
-        }
-});
-    
-}
+
        
        
 
 
 
     function getList(){
+        
         $.ajax({
             url:'https://api.backendless.com/36DC4E7A-2B33-40D0-A382-FD197E23A89B/BC24AD0C-019C-42A0-BAAD-229B8D8F7222/data/Movies',
             type: 'GET',
@@ -61,9 +44,10 @@ function Delete()
            success:function(data){
                var tab1 = $('#tb1movie tbody');
                tab1.empty();
-               $(data).each(function(i,e){
-                   tab1.append('<tr><td>'+e.Title+'</td><td>'+e.Year+'</td><td><button id="delbtn" onclick = "Delete()">Delete</button></td></tr>')
-               });
+                $(data).each(function(i,e){
+                   tab1.append('<tr><td>'+e.Title+'</td><td>'+e.Year+'</td><td><button id="delbtn" onclick = '+Delete(this.objectId)+'>Delete</button></td></tr>')
+                });
+               
            },
            error: function (error){
             alert(error);
@@ -71,7 +55,27 @@ function Delete()
         });
    
     } 
-
+    function Delete(id) 
+    {
+    $("#delbtn").click(function(){
+    {   var a ='https://api.backendless.com/36DC4E7A-2B33-40D0-A382-FD197E23A89B/BC24AD0C-019C-42A0-BAAD-229B8D8F7222/data/Movies/'+id;
+       $.ajax({
+            url:a,
+            type: 'DELETE',
+            success:function(resp, status, xhr){
+                alert("Deleted");
+                console.log(resp, status, xhr);
+                getList();
+            },
+            error:function(xhr){
+                alert("error");
+                console.log(JSON.parse(xhr.responseText), xhr);
+                getList();
+            }
+    });   
+    }
+});
+    }
     function reset(){
         $('#title').val('');
         $('#year').val('');
